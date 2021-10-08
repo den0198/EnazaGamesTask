22b35f679fb3f14eec0df9fdfc0f07221534d527
Салихов Даниил <sterlok_0198@outlook.com>
2021-10-09 01:36:57 +0600

    add first test

--- EnazaGamesTask.Tests/ContextTests.cs
+++ EnazaGamesTask.Tests/ContextTests.cs
@@ -1,7 +1,27 @@
-﻿namespace EnazaGamesTask.Tests1
+﻿using EnazaGamesTask.Tests.Infrastructure.Helpers;
+using Xunit;
+
+namespace EnazaGamesTask.Tests1
 {
-    public class ContextTests
+    public class ContextTests : IClassFixture<DbContextHelper>
     {
-        
+        private readonly DbContextHelper _fixture;
+
+        public ContextTests(DbContextHelper fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void ItShouldContextNotNul()
+        {
+            //arrange
+            var sut = _fixture.Context;
+            
+            //act
+            
+            //assert
+            Assert.NotNull(sut);
+        }
     }
 }
